e57949c8b0af2d0d4e690b0cff9c1643ab69cb3a
themostretarded <marcosresidentevil@hotmail.com>
2022-04-29 20:40:12 -0500

    avances de arreglos

---
+++ ArreglosDemo/ArreglosDemo/Circulo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArreglosDemo
+{
+    public class Circulo
+    {
+        public int radio;
+        public Circulo(int _radio)
+        {
+            radio = _radio;
+        }
+    }
+}
--- ArreglosDemo/ArreglosDemo/Program.cs
+++ ArreglosDemo/ArreglosDemo/Program.cs
@@ -10,6 +10,26 @@ namespace ArreglosDemo
     {
         static void Main(string[] args)
         {
+            //Inicializador de un arreglo
+            //int[] numeros = new int[4];
+            //Declaracion de arreglo
+            int[] numeros = {211,2,2,3 };
+
+
+            Circulo[] circulos = {new Circulo(20),
+                new Circulo(5) };
+
+            var cadenas = new[] {2,2,3,2.54 };
+            var personas = new[] {new{Nombre ="ada" },
+                new{ Nombre="hector"}
+                };
+
+            try {
+                int numero3 = numeros[10];
+            }
+            catch {
+                Console.WriteLine("Indice incorrecto");
+            }
         }
     }
 }
