995b3dbb6ba6716e2167f1373951242d0d3a8674
themostretarded <marcosresidentevil@hotmail.com>
2022-05-26 20:54:21 -0500

    nueva seccion

---
+++ AdministracionRecursosDemo/AdministracionRecursosDemo/FileManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdministracionRecursosDemo
+{
+    class FileManager
+    {
+        FileStream reader = null;
+        public FileManager(string filePath) {
+            reader = File.Open(filePath,FileMode.Open);
+            Byte[] b = new Byte[1024];
+            UTF8Encoding temp = new UTF8Encoding();
+            while (reader.Read(b,0,b.Length)>0) {
+                Console.WriteLine(temp.GetString(b));
+            }
+        }
+        ~FileManager() {
+            reader.Close();
+            System.Diagnostics.Trace.WriteLine("Archivo Cerrado");
+        }
+    }
+}
---
+++ AdministracionRecursosDemo/AdministracionRecursosDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdministracionRecursosDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileManager manager1 = new FileManager(@"C:\Users\Meer\Desktop\redaccion.txt");
+            FileManager manager2 = new FileManager(@"C:\Users\Meer\Desktop\redaccion.txt");
+            Console.ReadLine();
+        }
+    }
+}
