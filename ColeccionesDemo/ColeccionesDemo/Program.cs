8d9e900bc38ce1bb3f0a3bcaa557411e232bb658
themostretarded <marcosresidentevil@hotmail.com>
2022-06-14 20:32:16 -0500

    me quede en sortedlist

--- ColeccionesDemo/ColeccionesDemo/Program.cs
+++ ColeccionesDemo/ColeccionesDemo/Program.cs
@@ -10,33 +10,83 @@ namespace ColeccionesDemo
     {
         static void Main(string[] args)
         {
-            LinkedList<int> numeros = new LinkedList<int>();
-            //numeros.AddLast(3);
-            for (int i = 1; i <= 10; i++)
-            {
-                numeros.AddFirst(i);
-            }
-            var primero= numeros.AddFirst(300);
-            numeros.AddAfter(primero,200);
-            numeros.AddBefore(primero,250);
-            var nodo = numeros.First;
-            while (nodo!=null) {
-                Console.WriteLine(nodo.Value);
-                nodo = nodo.Next;
-            }
-            for (var nodo2=numeros.Last;nodo!=null;nodo2=nodo2.Previous) {
-                Console.WriteLine(nodo2.Value);
-            }
-            numeros.Remove(3);
-            numeros.RemoveLast();
-            numeros.RemoveFirst();
-            //numeros.AddLast(200);
-            foreach (var numero in numeros){
-                Console.WriteLine(numero);
-            }
+            //Dictionary<int, int> diccionario = new Dictionary<int, int>();
+            //Dictionary<string, string> frases = new Dictionary<String, String>();
+            //frases.Add("compayito", "pico revoloteo y ataco");
+            //frases.Add("Pajaro ", "Tieso");
+            //foreach (var elemento in frases)
+            //{
+            //    string autor = elemento.Key;
+            //    string frase = elemento.Value;
+            //    //cadena interpolada
+            //    Console.WriteLine($"{autor},{frase}");
+            //}
             
-
+            //frases["compayito"] = "sir cuncesion";
+            //foreach (var elemento in frases)
+            //{
+            //    string autor = elemento.Key;
+            //    string frase = elemento.Value;
+            //    //cadena interpolada
+            //    Console.WriteLine($"{autor},{frase}");
+            //}
+            //for (int i=1;i<=10;i++)
+            //{
+            //    diccionario.Add(i,i);
+            //}
             Console.ReadLine();
+            //Stack<int> numeros = new Stack<int>();
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    numeros.Push(i);
+            //}
+            //var elemento = numeros.Pop();
+            //while (numeros.Count > 0)
+            //{
+            //    Console.WriteLine(numeros.Pop());
+            //}
+            //foreach (var numero in numeros) 
+            //{
+            //    Console.WriteLine(numero);
+            //}
+            //Queue<int> numeros = new Queue<int>();
+            ////numeros.Enqueue(1);
+            //for (int i =1;i<=10;i++) {
+            //    numeros.Enqueue(i);
+            //}
+            //var elemento = numeros.Dequeue();
+            //while (numeros.Count >0)
+            //{
+            //    Console.WriteLine(numeros.Dequeue());
+            //}
+            ////foreach (var numero in numeros) 
+            ////{
+            ////    Console.WriteLine(numero);
+            ////}
+            //LinkedList<int> numeros = new LinkedList<int>();
+            ////numeros.AddLast(3);
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    numeros.AddFirst(i);
+            //}
+            //var primero= numeros.AddFirst(300);
+            //numeros.AddAfter(primero,200);
+            //numeros.AddBefore(primero,250);
+            //var nodo = numeros.First;
+            //while (nodo!=null) {
+            //    Console.WriteLine(nodo.Value);
+            //    nodo = nodo.Next;
+            //}
+            //for (var nodo2=numeros.Last;nodo!=null;nodo2=nodo2.Previous) {
+            //    Console.WriteLine(nodo2.Value);
+            //}
+            //numeros.Remove(3);
+            //numeros.RemoveLast();
+            //numeros.RemoveFirst();
+            ////numeros.AddLast(200);
+            //foreach (var numero in numeros){
+            //    Console.WriteLine(numero);
+            //}
             //int[] numeroArreglo = new int[10];
             //numeroArreglo[2] = 20;
             //List<int> numerosLista = new List<int>();
@@ -50,13 +100,11 @@ namespace ColeccionesDemo
             //numerosLista.RemoveAt(0);
             //numerosLista.RemoveRange(0,3);
             ////numerosLista.Clear();
-
             //int numero = numerosLista[0];
             //foreach (var elemento in numerosLista)
             //{
             //    Console.WriteLine(elemento);
             //}
-
         }
     }
 }
